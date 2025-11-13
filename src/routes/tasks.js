fb34d33b82a14ed3a3a0ecbe4a79b69b55f83ec4
Jim Kennedy <kennedyomata@gmail.com>
2025-11-13 15:27:07 +0300

    feat: enhance bulk task creation and add task retrieval by ID with error handling

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+import { ObjectId } from "mongodb"; //correctly query with _id
 
 //show db status
 router.get("/status", async (req, res) => {
@@ -49,41 +50,78 @@ router.post("/new", async (req, res) => {
 
 // InsertMany
 router.post("/bulk-new", async (req, res) => {
-    try{
-        const db = req.app.locals.db;
-        const tasksCollection = db.collection('tasks');
+  try {
+    const db = req.app.locals.db;
+    const tasksCollection = db.collection("tasks");
 
-        const newTasks = req.body.tasks; //array of tasks from request body
+    const newTasks = req.body.tasks; //array of tasks from request body
 
-        // data validation
-        if(!Array.isArray(newTasks) || newTasks === 0){
-            return res.status(400).json({
-                message: "Invalid request body. 'tasks' should be a non-empty array."
-            });
-        }
-
-        // new individual tasks array from request body
-        const tasksToInsert = newTasks.map(task => ({
-            //use spread operator to get task properties
-            ...task,
-            completed: false,
-            createdAt: new Date(),
-            dueDate: task.dueDate ? new Date(task.dueDate) : null,
-        }));
-
-        // insert multiple tasks into the collection
-        const InsertManyResult = await tasksCollection.insertMany(tasksToInsert);
-        res.status(201).json({
-            message: `${InsertManyResult.insertCount} Bulk tasks created`,
-            inserteIds: InsertManyResult.insertedIds,
-            tasks: tasksToInsert,
-        });
-    }catch(error){
-        console.error("Error creating bulk tasks:", error);
-        res.status(500).json({
-            message: "Failed to create bulk tasks",
-            error: error.message,
-        });
+    // data validation
+    if (!Array.isArray(newTasks) || newTasks === 0) {
+      return res.status(400).json({
+        message: "Invalid request body. 'tasks' should be a non-empty array.",
+      });
     }
-})
+
+    // new individual tasks array from request body
+    const tasksToInsert = newTasks.map((task) => ({
+      //use spread operator to get task properties
+      ...task,
+      completed: false,
+      createdAt: new Date(),
+      dueDate: task.dueDate ? new Date(task.dueDate) : null,
+    }));
+
+    // insert multiple tasks into the collection
+    const InsertManyResult = await tasksCollection.insertMany(tasksToInsert);
+    res.status(201).json({
+      message: `${InsertManyResult.insertCount} Bulk tasks created`,
+      inserteIds: InsertManyResult.insertedIds,
+      tasks: tasksToInsert,
+    });
+  } catch (error) {
+    console.error("Error creating bulk tasks:", error);
+    res.status(500).json({
+      message: "Failed to create bulk tasks",
+      error: error.message,
+    });
+  }
+});
+
+// FindOne - Read Operation - One task only
+// db.collection('collectionName').findOne(query, options)
+
+router.get("/gettask/:id", async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const taskCollection = db.collection("tasks");
+    const taskId = req.params.id;
+
+    //convert string ID to Mongo ObjectID
+    let taskObjectID;
+    try {
+      taskObjectID = new ObjectId(taskId);
+    } catch (error) {
+      return res.status(400).json({ message: "Invalid Task ID format" });
+    }
+
+    //find a single task matching the id
+    const task = await taskCollection.findOne({
+      _id: taskObjectID,
+    });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    console.error("Error finding task:", error);
+    res
+      .status(500)
+      .json({
+        message: `Failed to find task with id ${taskId}`,
+        error: error.message,
+      });
+  }
+});
+
 export default router;
