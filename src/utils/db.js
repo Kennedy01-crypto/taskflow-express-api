19d94956636c9e8bc0c29ee333c946c5f2084721
Jim Kennedy <kennedyomata@gmail.com>
2025-11-17 13:05:01 +0300

    feat: connected database via mongoose

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,60 +1,18 @@
 //import the "MongoClient" class from the "mongodb" package
 import { MongoClient } from "mongodb";
-
+import mongoose from "mongoose";
 // MongoDB connection URI
 const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/taskflowdb";
 
-// Variable to hold the database instance
-let dbInstance;
-
-
 // Function to connect to the MongoDB database
 async function connectionToDatabase() {
-  if (dbInstance) { // Return existing instance if already connected
-    return dbInstance;
-  }
-
   try {
-    const client = new MongoClient(uri); // Create a new MongoClient. 
-    await client.connect(); // Connect to the MongoDB server
-    console.log("Successfully connected to MongoDB");
-    dbInstance = client.db(); // Get the database instance
-    return dbInstance;
+    await mongoose.connect(uri); // Connect to the MongoDB via mongoose
+    console.log("Successfully connected via Mongoose to MongoDB");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
     throw error;
+    process.exit(1);
   }
 }
-
-
-// Function to get the database instance
-function getDb() {
-  if (!dbInstance) { //returns an error if the database is not initialized
-    throw new Error(
-      "Database not initialized. Call connectionToDatabase first."
-    );
-  }
-  return dbInstance;
-}
-//close the database connection gracefully
-async function closeDatabase() {
-  if (dbInstance) {
-    try {
-      const client = new MongoClient(uri); // in real world, you'd store the client instance
-      await client.connect();
-      await client.close();
-      console.log("MongoDB connection closed.");
-      dbInstance = null;
-    } catch (error) {
-      console.error("Error closing MongoDB connection:", error);
-      throw error;
-    }
-  }
-}
-
-// module exports
-export {
-  connectionToDatabase,
-  getDb,
-  closeDatabase,
-};
+export default connectionToDatabase;
