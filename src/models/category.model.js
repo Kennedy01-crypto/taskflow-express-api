1e440c7fd95840d71b968162755eb216c26fc73d
Jim Kennedy <kennedyomata@gmail.com>
2025-11-18 14:28:47 +0300

    feat: schema definition for models

---
+++ src/models/category.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    maxlength: [200, "Description cannot exceed 200 characters"],
+    trim: true,
+  },
+  colorCode: {
+    type: String,
+    validate: {
+        //example: #RRGGBB
+        validator: function (v) {
+            return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid color code!`,
+    }
+  }
+}, {timestamps: true});
+
+export default mongoose.model("Category", categorySchema);
