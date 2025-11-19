9e54d6fcc68f1c19326b0531ea397a2be24a8454
Jim Kennedy <kennedyomata@gmail.com>
2025-11-19 15:20:18 +0300

    fix: updated application logic to enhace separation of concerns

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
-import tasksRouter from "./routes/tasksRoutes.js";
 import mongoose from "mongoose";
-import categoryModel from "./models/category.model.js";
-import taskModel from "./models/task.model.js";
+//db import
 import connectionToDatabase from "./utils/db.js";
+//routes import
+import tasksRouter from "./routes/tasksRoutes.js";
+import categoryRouter from "./routes/categoryRoutes.js";
+//others
 import dotenv from "dotenv";
 
 // load env variables
@@ -18,50 +20,24 @@ connectionToDatabase();
 
 // import your routes here and use them
 app.use("/api/tasks", tasksRouter);
+app.use("/api/categories", categoryRouter);
 
-app.post("/api/tasks", async (req, res) => {
-  try {
-    const dummyUserId = new mongoose.Types.ObjectId(); // Generate a new ObjectId
-    const newTask = new taskModel({
-      title: req.body.title,
-      description: req.body.description,
-      status: req.body.status,
-      dueDate: req.body.dueDate,
-      isCompleted: req.body.isCompleted,
-      priority: req.body.priority,
-      userId: req.body.userId || dummyUserId, // Use provided userId or our dummy one
-    });
-    const savedTask = await newTask.save();
-    res.status(201).json(savedTask);
-  } catch (error) {
-    // Mongoose validation errors will have a 'name' of 'ValidationError'
-    if (error.name === "ValidationError") {
-      const errors = {};
-      for (let field in error.errors) {
-        errors[field] = error.errors[field].message;
-      }
-      return res.status(400).json({ message: "Validation failed", errors });
-    }
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 //post to categories
-app.post("/api/categories", async (req, res) => {
-  try {
-    const newCategory = new categoryModel({
-      name: req.body.name,
-      description: req.body.description,
-      colorCode: req.body.colorCode,
-    });
-    const savedCategory = await newCategory.save();
-    res.status(201).json(savedCategory);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: `Error posting category ${err.message}` });
-  }
-});
+// app.post("/api/categories", async (req, res) => {
+//   try {
+//     const newCategory = new categoryModel({
+//       name: req.body.name,
+//       description: req.body.description,
+//       colorCode: req.body.colorCode,
+//     });
+//     const savedCategory = await newCategory.save();
+//     res.status(201).json(savedCategory);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: `Error posting category ${err.message}` });
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.send("✅TaskFlow API is running!");
