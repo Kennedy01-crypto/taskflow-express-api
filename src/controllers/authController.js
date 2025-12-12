fa514a7f469fccce5e6c3c9c036c234116651b0a
Jim Kennedy <kennedyomata@gmail.com>
2025-12-12 12:53:06 +0300

    feat(auth): --added login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 import AppError from "../utils/appError.js";
 import User from "../models/user.model.js";
 // import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 // User Registration Controller
 export const registerUser = async (req, res, next) => {
@@ -30,4 +30,46 @@ export const registerUser = async (req, res, next) => {
   }
 };
 
-export const loginUser = async (req, res, next) => {};
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //Basic input validation
+    if (!email || !password) {
+      return next(new AppError("Please provide email and password", 400));
+    }
+
+    //Find User by email
+    // explicitly select the password because we set select: false in the schema
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      //return a generic error message for security reasons
+      //this prevents attackers from knowing if an email exists in the system
+      return next(new AppError("Invalid email or password", 401)); //unauthorized
+    }
+
+    //Compare provided password with hashed password
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      return next(new AppError("Invalid email or password", 401)); //unauthorized
+    }
+
+    //Generate JWT
+    const token = user.createJWT();
+
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
+      data: {
+        user: {
+          userId: user._id,
+          email: user.email,
+        },
+        token,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
